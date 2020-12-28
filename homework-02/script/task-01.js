e5e378bb98c43e7ab2e85384cad65fa7c54e62f1
Ivan Chuchulski <zake6@abv.bg>
2020-12-28 19:36:36 +0200

    work on hw2

--- homework-02/script/task-01.js
+++ homework-02/script/task-01.js
@@ -1,15 +1,19 @@
 (function () {
-    let goodInput = "ggghhhmmmdddlll";
+    let good1 = "ggghhhmmmdddlll";
+    let good2 = "aa";
 
     let bad1 = "aaabbbcccc";
     let bad2 = "bbabb";
     let bad3 = "abbc";
-
     let bad4 = "aaabccc";
-    let uglyInput2 = "bbaabb";
+
+    let ugly1 = "aabbbb";
+    let ugly2 = "aabbbbc";
+    let ugly3 = "abbccc";
 
     console.log("---good---");
-    solution(goodInput);
+    solution(good1);
+    solution(good2);
 
     console.log("---bad---");
     solution(bad1);
@@ -18,10 +22,17 @@
     solution(bad4);
 
     console.log("---ugly---");
-    solution(uglyInput2);
+    solution(ugly1);
+    solution(ugly2);
+    solution(ugly3);
 })();
 
 function solution(input) {
+    sol2(input);
+}
+
+// still incorrect
+function sol1(input) {
     let inputToLowerCase = input.toLowerCase();
     let symbolsOccurrences = new Map();
 
@@ -34,11 +45,45 @@ function solution(input) {
         }
     }
 
-    let occurrences = [];
+    let occs = new Set();
 
     for (let value of symbolsOccurrences.values()) {
-        occurrences.push(value);
+        occs.add(value);
     }
+
+    if (occs.size === 1) {
+        console.log("GOOD");
+        return;
+    }
+
+    if (occs.size > 2) {
+        console.log("UGLY");
+        return;
+    }
+
+    let occurrencesArr = [];
+    for (let value of occs.keys()) {
+        occurrencesArr.push(value);
+    }
+
+    if (occurrencesArr.some((element) => element === 1)) {
+        console.log("BAD");
+        return;
+    }
+
+    let difference = Math.abs(occurrencesArr[0] - occurrencesArr[1]);
+
+    if (difference === 1) {
+        console.log("BAD");
+    } else {
+        console.log("UGLY");
+    }
+
+}
+
+function sol2(input) {
+    let symbolsOccurrences = buildOccurrencesMap(input);
+    let occurrences = Array.from(symbolsOccurrences.values());
 
     if (allArrayElementsAreEqual(occurrences)) {
         console.log("GOOD");
@@ -49,7 +94,6 @@ function solution(input) {
 
     for (let i = 0; i < occurrences.length; i++) {
         let toFillBackElement = false;
-        let currentElement = occurrences[i];
 
         occurrences[i] -= 1;
 
@@ -64,7 +108,7 @@ function solution(input) {
         }
 
         if (toFillBackElement) {
-            occurrences.splice(i, 0, currentElement);
+            occurrences.splice(i, 0, 0);
         }
 
         occurrences[i] += 1;
@@ -75,6 +119,22 @@ function solution(input) {
     } else {
         console.log("UGLY");
     }
+}
+
+function buildOccurrencesMap(input) {
+    let inputToLowerCase = input.toLowerCase();
+    let symbolsOccurrences = new Map();
+
+    for (const symbol of inputToLowerCase) {
+        if (symbolsOccurrences.has(symbol)) {
+            let currentCount = symbolsOccurrences.get(symbol);
+            symbolsOccurrences.set(symbol, currentCount + 1);
+        } else {
+            symbolsOccurrences.set(symbol, 1);
+        }
+    }
+
+    return symbolsOccurrences;
 }
 
 function allArrayElementsAreEqual(arr) {
